a359d31e927a16e9871563acc44b24687e45844f
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 13:11:02 +0300

    exersises

--- Week 2/Day 1/ExerciseXP/Exersize9.py
+++ Week 2/Day 1/ExerciseXP/Exersize9.py
@@ -1,6 +1,6 @@
-print("enter your height in cm >>>")
-height = int(input())
+height = input("enter your height in cm >>>")
+height = int(height)
 if height > 145:
     print("You can ride!")
 else:
-    print("You can't ride!")
+    print("You can't ride, you need to gow some more!")
