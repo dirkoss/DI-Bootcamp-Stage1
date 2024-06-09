21bb38d03c45c7089ddf0694d5fab42e4acac701
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 09:09:46 +0300

    Folders

--- Week 2/Day 1/first.py
+++ Week 2/Day 1/first.py
@@ -1,8 +1,8 @@
+import time
 print ("Hello, World!")
 print ("good morning")
 item = 'banana'
 print(item)
-
 print("hello " + item)
 
 a = 2
@@ -12,3 +12,18 @@ addition=  a + b
 print(addition)
 multiplication = a * b
 print(multiplication)
+
+print(item + str(a))
+
+while True:
+    print("hello")
+    break
+
+print("hello 2")
+
+counter = 0
+while True:
+    print("hello")
+    counter += 1
+    if counter == 5:
+        break
