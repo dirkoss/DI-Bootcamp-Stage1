703c34820d7898baa36e40dfc461199a99c81661
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 14:17:54 +0300

    Update

--- Week 2/Day 1/ExerciseXP/Exersize5.py
+++ Week 2/Day 1/ExerciseXP/Exersize5.py
@@ -1,9 +1,10 @@
 name = 'Dirk'
 age = 33
 shoe_size = 45
+info = f"Hi, my name is {name} and I am {age} years old. My shoe size is {shoe_size}" # I do not understand the syntax=error of this line
 
 #Three possible ways to print the same string
 
 print(info)
 print("Hi, my name is %s and I am %d years old. My shoe size is %d" % (name, age, shoe_size))
-info = f"Hi, my name is {name} and I am {age} years old. My shoe size is {shoe_size}" # I do not understand the syntax=error of this line
+print("Hi, my name is {} and I am {} years old. My shoe size is {}".format(name, age, shoe_size))
