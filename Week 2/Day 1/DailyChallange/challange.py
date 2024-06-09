23d137c72ad23d7e66bfa2a7096c8d032b89f901
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 13:40:04 +0300

    daily challange

--- Week 2/Day 1/DailyChallange/challange.py
+++ Week 2/Day 1/DailyChallange/challange.py
@@ -1,3 +1,33 @@
-String = input("Enter a string: ")
-if String = count(ch)
-    print("Yes")
+# Instructions
+
+#     Using the input function, ask the user for a string. The string must be 10 characters long.
+#         If it’s less than 10 characters, print a message which states “string not long enough”.
+#         If it’s more than 10 characters, print a message which states “string too long”.
+#         If it’s 10 characters, print a message which states “perfect string” and continue the exercise.
+
+#     Then, print the first and last characters of the given text.
+
+#     Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+
+#     The user enters "HelloWorld"
+#     Then, you have to construct the string character by character
+#     H
+#     He
+#     Hel
+#     ... etc
+#     HelloWorld
+
+
+User_Word = input("Please enter a string of 10 characters >>>")
+
+if len(User_Word) < 10:
+    print ("string not long enough") 
+elif len(User_Word) > 10:
+    print ("string too long")
+else:
+    print ("perfect string")
+
+    print(User_Word[0] + User_Word[9])
+
+for letter in range(1, len(User_Word) + 1):
+        print(User_Word[0:letter])
