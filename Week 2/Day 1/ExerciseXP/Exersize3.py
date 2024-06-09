e34054e1c813ecadabf0366fcb3259476b13c043
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 14:02:04 +0300

    update exercise 3

--- Week 2/Day 1/ExerciseXP/Exersize3.py
+++ Week 2/Day 1/ExerciseXP/Exersize3.py
@@ -1,5 +1,5 @@
-False # 5 is larger than 3
-True # it's equal
-False # string versus integer
-False # it's equal
-False # capital dont match
+False   # 5 is larger than 3
+True    # it's equal
+False   # string versus integer
+False   # it's equal
+False   # capital dont match
