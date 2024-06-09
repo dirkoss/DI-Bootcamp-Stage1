8d01fd6b3255a84768c1b68f07b2bb5b0e443626
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 08:41:21 +0300

    Create first.py

---
+++ Week 2/Day 1/first.py
@@ -0,0 +1,14 @@
+print ("Hello, World!")
+print ("good morning")
+item = 'banana'
+print(item)
+
+print("hello " + item)
+
+a = 2
+b = 3
+
+addition=  a + b
+print(addition)
+multiplication = a * b
+print(multiplication)
