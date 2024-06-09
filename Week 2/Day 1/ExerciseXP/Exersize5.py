ce5708f4346b735987115b80274c253ad9942216
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-09 13:20:15 +0300

    exersize5

--- Week 2/Day 1/ExerciseXP/Exersize5.py
+++ Week 2/Day 1/ExerciseXP/Exersize5.py
@@ -1,7 +1,7 @@
 name = 'Dirk'
 age = 33
 shoe_size = 45
-info = "Hi, my name is " + name  + " and I am " + str(age) + " years old. My shoe size is " + str(shoe_size)
+info = f"Hi, my name is {name} and I am {age} years old. My shoe size is {shoe_size}"
 
 #Three possible ways to print the same string
 
--- Week 2/Day 1/ExerciseXP/test.py
+++ Week 2/Day 1/ExerciseXP/test.py
@@ -4,20 +4,27 @@
 # elif int(input()) = :
 #     print("Fail")
 
-
-user_number = input("write down number between 0 - 100 >>>")
-
-# print(type(user_number))
-
-user_number = int(user_number)
-
-if user_number > 1 and user_number < 100:
-if user_number % 3 == 0 and user_number % 5 == 0:
-    print("FizzBuzz")
-elif user_number % 3 == 0:
-    print("Fizz")
-elif user_number % 5 == 0:
+number = int(input("Enter a number 1-100"))
+if(number % 3 == 0 ):
+    print("Fizz", end="")
+if(number % 5 == 0 ):
     print("Buzz")
 
-else:
-    print("Write down a correct number")
+
+
+# user_number = input("write down number between 0 - 100 >>>")
+
+# # print(type(user_number))
+
+# user_number = int(user_number)
+
+# if user_number > 1 and user_number < 100:
+# if user_number % 3 == 0 and user_number % 5 == 0:
+#     print("FizzBuzz")
+# elif user_number % 3 == 0:
+#     print("Fizz")
+# elif user_number % 5 == 0:
+#     print("Buzz")
+
+# else:
+#     print("Write down a correct number")
