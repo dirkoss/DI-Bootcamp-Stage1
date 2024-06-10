4f36900c66b969c3dd0f238e925b8abc4a8bd199
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 13:57:49 +0300

    6

--- Week 2/Day 2/ExerciseXP/Exersize5.py
+++ Week 2/Day 2/ExerciseXP/Exersize5.py
@@ -1,5 +1,5 @@
-for i in range(20):
-    print(i+1)
+numbers = [i for i in range(1,21)]
+print(numbers)
 
-for i in range(i, 20, 2):
-    print(i+1)
+numbers = [i for i in range(1,21) if i % 2 == 0]
+print(numbers)
---
+++ Week 2/Day 2/ExerciseXP/Exersize6.py
@@ -0,0 +1,5 @@
+while True:
+    Username = input("Please enter your username: ")
+    if Username == "Dirk":
+        print("Welcome, Dirk!")
+        break
