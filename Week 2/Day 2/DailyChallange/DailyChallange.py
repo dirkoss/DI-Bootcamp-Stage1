f81596ddd0c0670c6ad136442669af61d1c32e4a
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 16:34:24 +0300

    finish DC

---
+++ Week 2/Day 2/DailyChallange/DailyChallange.py
@@ -0,0 +1,12 @@
+
+Number_Length = input("Enter a random number and the length of the desired sequence: ")
+
+#Split the input into two parts and qualisify the string as two integers
+Number_Length_Split = Number_Length.split(",")
+Number = int(Number_Length_Split[0])
+Lenght = int(Number_Length_Split[1])
+
+Sequence = []
+for i in range(1, Lenght + 1) :
+    Sequence.append(Number * i)
+print(Sequence)
