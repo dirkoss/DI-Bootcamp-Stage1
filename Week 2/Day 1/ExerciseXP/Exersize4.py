c2fcae20931546b59039d4a7341d9f98164da124
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 10:35:57 +0300

    remove syntax error

--- Week 2/Day 1/ExerciseXP/Exersize4.py
+++ Week 2/Day 1/ExerciseXP/Exersize4.py
@@ -1,3 +1,3 @@
 computer_brand = "Apple"
 print("I have an " + computer_brand + " computer")
-print(f"I have an {computer_brand} computer")        # I do not understand the syntax-error of this line
+print(f"I have an {computer_brand} computer")        # syntax error
--- Week 2/Day 2/lists.py
+++ Week 2/Day 2/lists.py
@@ -1,8 +1,16 @@
 # List = Array
 
-a_list = [1, 2, 3, 4, 5, [1, 2, 3]]
-inner_list = a_list[-1]
-print(inner_list)
-print(inner_list[0])
-# print(len(a_list))
-# print(a_list[4])
+# a_list = [1, 2, 3, 4, 5, [1, 2, 3]]
+# inner_list = a_list[-1]
+# print(inner_list)
+# print(inner_list[0])
+# # print(len(a_list))
+# # print(a_list[4])
+
+numbers = [1, 2, 3, 4, 5]
+
+sum = 0 
+for number in numbers:
+    sum += number   
+
+print(sum)
