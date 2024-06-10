121cfea719d2f4768ff15fb275d2ab56e29ddfb7
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 11:29:13 +0300

    sets/lists

---
+++ Week 2/Day 2/sets.py
@@ -0,0 +1,7 @@
+# Sets are like lists, but cannot be indexed. They are unordered and do not allow duplicates.
+# Use sets when 
+    # 1. All you need is a collection of unique elements.
+    # 2. You don't care about the order of the elements.
+    # 3. You don't need to access the elements by index.
+    # 4. You don't need to access the elements by key.
+    # 5. You don't need to access the elements by value.
--- Week 2/Day 2/tuples.py
+++ Week 2/Day 2/tuples.py
@@ -1,1 +1,9 @@
 # typles are not mutiple = they cannot be changed
+
+a_tuple = (1, "hans", 2.5)
+a_tuple.count
+
+for element in a_tuple:
+    print(element)
+
+# print(a_tuple.count(2.5))
