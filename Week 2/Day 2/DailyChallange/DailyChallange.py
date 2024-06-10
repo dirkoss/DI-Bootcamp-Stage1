56bd770d86f3799dde752d5d6369e9baf7309fd1
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 16:36:25 +0300

    Update DC

--- Week 2/Day 2/DailyChallange/DailyChallange.py
+++ Week 2/Day 2/DailyChallange/DailyChallange.py
@@ -1,7 +1,7 @@
 
 Number_Length = input("Enter a random number and the length of the desired sequence: ")
 
-#Split the input into two parts and qualisify the string as two integers
+# Split the input into two parts and qualisify the string as two integers
 Number_Length_Split = Number_Length.split(",")
 Number = int(Number_Length_Split[0])
 Lenght = int(Number_Length_Split[1])
