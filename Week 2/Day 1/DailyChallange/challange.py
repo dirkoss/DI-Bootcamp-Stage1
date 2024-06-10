118f667ad1559566fd44bb715456f28ec933bce6
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 10:01:53 +0300

    update challange

--- Week 2/Day 1/DailyChallange/challange.py
+++ Week 2/Day 1/DailyChallange/challange.py
@@ -1,11 +1,11 @@
 User_Word = input("Please enter a string of 10 characters >>>")
 
-if len(User_Word) < 10:
-    print ("string not long enough") 
-elif len(User_Word) > 10:
-    print ("string too long")
+if (len(User_Word) < 10):
+    print ("String not long enough") 
+elif (len(User_Word) > 10):
+    print ("String too long")
 else:
-    print ("perfect string")
+    print ("Perfect String")
 
     print(User_Word[0] + User_Word[9])
 
