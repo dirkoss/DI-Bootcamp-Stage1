ca1ac04cb3cced15bbc0c184dd1819b9bd0167f2
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 10:22:26 +0300

    Exercise update

--- Week 2/Day 1/DailyChallange/challange.py
+++ Week 2/Day 1/DailyChallange/challange.py
@@ -6,8 +6,11 @@ elif (len(User_Word) > 10):
     print ("String too long")
 else:
     print ("Perfect String")
+    first_char = User_Word[0]
+    last_char = User_Word[-1]
 
-    print(User_Word[0] + User_Word[9])
+    print("The first character is: " + first_char)
+    print("The last character is: " + last_char)
 
-for letter in range(1, len(User_Word)+1):
-        print(User_Word[0:letter])
+    for i in range(1, len(User_Word)+1):
+        print(User_Word[:i])
