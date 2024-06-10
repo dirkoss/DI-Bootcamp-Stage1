0f7729d7ed6fef5f32174f923fe99cd9b050b9be
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-10 11:10:27 +0300

    Update lists.py

--- Week 2/Day 2/lists.py
+++ Week 2/Day 2/lists.py
@@ -7,10 +7,18 @@
 # # print(len(a_list))
 # # print(a_list[4])
 
-numbers = [1, 2, 3, 4, 5]
+# numbers = [1, 2, 3, 4, 5]
 
-sum = 0 
-for number in numbers:
-    sum += number   
+# sum = 0 
+# for number in numbers:
+#     sum += number   
 
-print(sum)
+# print(sum)
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+
+for i in range(len(list1)):
+    if list1[i] == 20:
+        list1[i] = 200
+        print(list1)
+        break
