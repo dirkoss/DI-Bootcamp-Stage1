792624b37b7da9b275b83ee2921b8dd689ce5d60
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-11 10:42:42 +0300

    Dict

---
+++ Week 2/Day 3/dictionaries_intro.py
@@ -0,0 +1,24 @@
+# Dictionaries in python are a collection of key-value pairs. They are unordered, changeable and indexed.
+# Keys are always unique in dictionary like a set.
+# They are written with curly brackets and have keys and values. The keys are unique and the values can be of any data type.
+    # Lists are indexed based collections of items. They are ordered, changeable and allow duplicates. 
+    # They are written with square brackets and the items are separated by commas.
+
+# a_dictionary = {"Sun":"a star", "Earth":"a planet", "Moon":"a satellite"}
+
+# personal_information = {
+#     "name": "Dirk",
+#     "age": 30,
+#     "city": "Tel Aviv"
+# }
+# print(personal_information["name", "age", "city"])
+
+dict = {
+"country" : "India",
+"continent" : "Asia",
+"Other_name" : "Bharat"
+}
+x = dict["Other_name"]
+print(x)
+
+
