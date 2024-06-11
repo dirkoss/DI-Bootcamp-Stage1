cd67f0b0e6a37e8912d4e5a06a8cc835343ce116
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-11 10:59:25 +0300

    update dict

--- Week 2/Day 3/dictionaties_adding.py
+++ Week 2/Day 3/dictionaties_adding.py
@@ -1,1 +1,16 @@
 #add
+
+personal_information_dirk = {
+    "name": "Dirk",
+    "age": 30,
+    "city": "Tel Aviv"
+    }
+
+personal_information_dirk["phone_number"] = "+97255943054"
+print(personal_information_dirk)
+
+addtional_personal_information_dirk = {"Birthday": "01/01/1991", "Favourite Fruit": "Kiwi"}
+
+personal_information_dirk.update(addtional_personal_information_dirk)
+print(personal_information_dirk)
+
