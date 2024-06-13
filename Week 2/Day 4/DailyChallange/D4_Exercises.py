f5bad60b16e96f2d454a5a09045834fd6ee353ca
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-13 09:23:15 +0300

    D4_Exercises

---
+++ Week 2/Day 4/DailyChallange/D4_Exercises.py
@@ -0,0 +1,54 @@
+# EXERCISE 1
+# Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
+# Call the function, and make sure the message displays correctly.
+
+# EXERCISE 2
+# 1. Write a function called favorite_book() that accepts one parameter called title.
+# 2. The function should print a message, such as "One of my favorite books is <title>".
+# For example: “One of my favorite books is Alice in Wonderland”
+# 3. Call the function, make sure to include a book title as an argument when calling the function
+
+# EXERCISE 3
+# 1. Write a function called describe_city() that accepts the name of a city and its country as parameters.
+# 2. The function should print a simple sentence, such as "<city> is in <country>".
+# 3. For example “Reykjavik is in Iceland”
+# Give the country parameter a default value.
+# 4. Call your function.
+
+
+# EXERCISE 4
+# 1. Create a function that accepts a number between 1 and 100 and generates another number randomly between 1 and 100. Use the random module.
+# Compare the two numbers, if it’s the same number, display a success message, otherwise show a fail message and display both numbers.
+
+
+# EXERCISE 5
+# 1. Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+# 2. The function should print a sentence summarizing the size of the shirt and the message printed on it, such as "The size of the shirt is <size> and the text is <text>"
+# 3. Call the function make_shirt().
+
+# 4. Modify the make_shirt() function so that shirts are large by default with a message that reads “I love Python” by default.
+# 5. Call the function, in order to make a large shirt with the default message
+# 6. Make medium shirt with the default message
+# 7. Make a shirt of any size with a different message.
+
+
+# EXERCISE 6
+# Using this list of magician’s names
+
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+# 1. Create a function called show_magicians(), which prints the name of each magician in the list.
+# 2. Write a function called make_great() that modifies the original list of magicians by adding the phrase "the Great" to each magician’s name.
+# 3. Call the function make_great().
+# 4. Call the function show_magicians() to see that the list has actually been modified.
+
+# EXERCISE 7
+
+
+# EXERCISE 8
+
+
+# 1. Create a function that asks the questions to the user, and check his answers. Track the number of correct, incorrect answers. Create a list of wrong_answers
+# 2. Create a function that informs the user of his number of correct/incorrect answers.
+# 3. Bonus : display to the user the questions he answered wrong, his answer, and the correct answer.
+# If he had more then 3 wrong answers, ask him to play again.
