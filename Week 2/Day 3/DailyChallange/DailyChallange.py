a71aae87c3f91a205ba6811fec962c38fa514085
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-13 09:10:49 +0300

    DC

--- Week 2/Day 3/DailyChallange/DailyChallange.py
+++ Week 2/Day 3/DailyChallange/DailyChallange.py
@@ -21,7 +21,39 @@ print(Word)
 
 # CHALLANGE 2
 
-# 1. Create a program that prints a list of the items you can afford in the store with the money you have in your wallet.
+# 1. Create a program that prints a list of the items you can afford in the store with the money you have in your Wallet.
 #     Sort the list in alphabetical order.
 #     Return “Nothing” if you can’t afford anything from the store.
 #     Hint : make sure to convert the amount from dollars to numbers. Create a program that deletes the $ sign, and the comma (for thousands)
+
+Items = {
+  "Eggs": "1",
+  "Honey": "10",
+  "Milk": "5",
+}
+indx = 0
+Money_Wallet = []
+Wallet = "300"
+
+for char in Wallet:
+    if char.isdigit() == False:
+        Wallet = Wallet.replace(char, "")
+Wallet = int(Wallet)
+
+
+for key in Items:
+    for value in Items.values():
+        for char in value:
+            if char.isdigit() == False:
+                Items[key] = Items[key].replace(char, "")
+
+
+for key in Items:
+    Items[key] = int(Items[key])
+    if Items[key] < Wallet:
+        Money_Wallet.append(key)
+if len(Money_Wallet) == 0:
+    print("Broke")
+else:
+    Money_Wallet.sort()
+    print(Money_Wallet)
