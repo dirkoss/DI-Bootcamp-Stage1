1f4994522355829b7230cc8afffabfa8c168ee16
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-13 09:56:00 +0300

    Update DailyChallange.py

--- Week 2/Day 3/DailyChallange/DailyChallange.py
+++ Week 2/Day 3/DailyChallange/DailyChallange.py
@@ -1,6 +1,6 @@
 # CHALLANGE 1
 
-# 1. Ask a user for a word
+#   1. Ask a user for a word
 #     Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
 
 #     Make sure the letters are the keys.
@@ -22,10 +22,9 @@ for index, letter in enumerate(word):
 print(dictonary)
 
 
-
 # CHALLANGE 2
 
-# 1. Create a program that prints a list of the items you can afford in the store with the money you have in your Wallet.
+#   1. Create a program that prints a list of the items you can afford in the store with the money you have in your Wallet.
 #     Sort the list in alphabetical order.
 #     Return “Nothing” if you can’t afford anything from the store.
 #     Hint : make sure to convert the amount from dollars to numbers. Create a program that deletes the $ sign, and the comma (for thousands)
