a219383a17aaa8d9dd3494f6bdb52ebb0fe1dab8
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-16 11:17:15 +0300

    Exercise3

--- Week 3/Day 1/ExerciseXP/Exercises.py
+++ Week 3/Day 1/ExerciseXP/Exercises.py
@@ -78,6 +78,20 @@ if davids_dog.height > sarahs_dog.height:
 # all that glitters is gold
 # and she’s buying a stairway to heaven
 
+
+class Song:
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+stairway = Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+
+stairway.sing_me_a_song()
+
+
 # EXERCISE 4
 # 1. Create a class called Zoo.
 # 2. In this class create a method __init__ that takes one parameter: zoo_name.
