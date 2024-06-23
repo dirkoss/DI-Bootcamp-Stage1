39beee2f677ec1e493d25636c4373e9cc5d1d34c
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-23 18:08:13 +0300

    Update DC

--- Week 4/Day 1/DailyChallange/DC.sql
+++ Week 4/Day 1/DailyChallange/DC.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(*) FROM actors;
 
-I GET an error message because I specified NOT NULL as an attribuen in the table data type.
+I GET an error message because I specified NOT NULL as an attribute in the table data type.
