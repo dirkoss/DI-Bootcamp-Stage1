f424dcd3b6a09a5e00103e1390679284d3d6daa1
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-25 13:31:34 +0300

    Update

--- Week 4/Day 3/ExerciseXP/Exercise.sql
+++ Week 4/Day 3/ExerciseXP/Exercise.sql
@@ -21,6 +21,16 @@ VALUES ('The Departed'),('Star Wars'), ('Troy'), ('RoboCop')
 
 -- EXERCISE 5.
 
+CREATE TABLE customer_reviews (
+review_id SERIAL PRIMARY KEY NOT NULL,
+film_id INT references new_film(id) ON DELETE CASCADE,
+language_id INT references language(language_id),
+title VARCHAR(50),
+score INT CHECK (score >= 0),
+review_text text,
+last_update date
+);
+
 
 -- EXERCISE 6.
 
