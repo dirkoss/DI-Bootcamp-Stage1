9b05bc46bc0992549fb4e52b40f17282b044de57
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-06-29 06:38:50 +0300

    Update oop.py

--- oop.py
+++ oop.py
@@ -9,6 +9,10 @@ class Employee:
         return self.first + " " + self.last
 
 Employee1 = Employee("John", "Doe", 25)
+Employee2 = Employee("Jane", "Doe", 30)
 
+print(Employee.full_name(Employee1))
 print(Employee1.full_name())
 print(Employee1.age)
+
+
