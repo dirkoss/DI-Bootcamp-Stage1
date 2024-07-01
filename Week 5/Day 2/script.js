11d352e03f880b6a1edbefa7c5051fc14f8e5bbb
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-01 10:30:56 +0300

    Update script.js

--- Week 5/Day 2/script.js
+++ Week 5/Day 2/script.js
@@ -25,3 +25,15 @@ Kebab Case = "my-variable"
 function myFunction() {
   // code
 }
+
+// Key Words
+// break, case, catch, class, const, continue, debugger, default, delete, do, else, export, extends, finally, for, function, if, import, in, instanceof, new, return, super, switch, this, throw, try, typeof, var, void, while, with, yield
+
+// Conditional Statements
+if (condition) {
+  // code
+}
+
+console.log("Hello World");
+console.error("This is an error");
+console.warn("This is a warning");
