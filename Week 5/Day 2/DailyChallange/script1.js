a2c0b5c71e4a9bfcad1a04a29e6d9957335819b2
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-01 17:43:40 +0300

    Update script1.js

--- Week 5/Day 2/DailyChallange/script1.js
+++ Week 5/Day 2/DailyChallange/script1.js
@@ -3,8 +3,8 @@ let wordNot = sentence.indexOf("not")
 let wordBad = sentence.indexOf("bad")
 if (wordNot < wordBad && wordBad != -1 && wordNot != -1) {
     let old = sentence.slice(wordNot, wordBad + 3)
-    let new = sentence.replace(old, "good")
-    console.log(new);
+    let new1 = sentence.replace(old, "good")
+    console.log(new1);
 
 } else {
     console.log(sentence)
