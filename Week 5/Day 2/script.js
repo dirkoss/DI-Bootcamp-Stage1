09e930c60497a2d8f8d34c91d0f63cf295b0b176
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-01 10:18:20 +0300

    explenation

---
+++ Week 5/Day 2/script.js
@@ -0,0 +1,27 @@
+// Variables
+var = variable, can be redeclared 
+let = block scope, can be updated
+const = constant, cannot be updated
+
+// Data Types
+String = "Hello"
+Number = 123, or 123.456
+Boolean = true, false
+
+// Operators
+Arithmetic Operators = +, -, *, /, %, ++, --
+Assignment Operators = =, +=, -=, *=, /=, %=
+Comparison Operators = ==, ===, !=, !==, >, <, >=, <=
+Logical Operators = &&, ||, !
+
+// Nameing
+// Dont start with numbers or special characters
+Camel Case = "myVariable"
+Pascal Case = "MyVariable"
+Snake Case = "my_variable"
+Kebab Case = "my-variable"
+
+// Functions
+function myFunction() {
+  // code
+}
