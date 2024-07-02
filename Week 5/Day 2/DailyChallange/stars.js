f1f547c2a6623b420b44af2462357bc7c7c4084b
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-02 09:27:05 +0300

    Update stars.js

--- Week 5/Day 2/DailyChallange/stars.js
+++ Week 5/Day 2/DailyChallange/stars.js
@@ -18,11 +18,11 @@ for (let i = 0; i <7 1; i++) {
 
 
 // 2.
-let pattern = "*"
+let star = "*"
 
 for (let i = 1; i<7; i++) {
-    for (let j = 0; j<=i; j++) {
-        console.log(pattern.repeat(i));
+    for (let x = 0; x<=i; x++) {
+        console.log(star.repeat(i));
     }
 }
 
