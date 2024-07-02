0c506df64cb678f7591891bd7cda5c49bd7ba400
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-02 09:25:29 +0300

    DC

--- Week 5/Day 2/DailyChallange/stars.js
+++ Week 5/Day 2/DailyChallange/stars.js
@@ -1,17 +1,17 @@
 // 1.
 
-for (let index = 1; index = 6; index++) {
-    if (index == 1) {
+for (let i = 0; i <7 1; i++) {
+    if (i == 1) {
         console.log("*");
-    } else if (index == 2){
+    } else if (i == 2){
         console.log("**");
-    } else if (index == 3){
+    } else if (i == 3){
         console.log("***");
-    } else if (index == 4){
+    } else if (i == 4){
         console.log("****");
-    } else if (index == 5){
+    } else if (i == 5){
         console.log("*****");
-    } else if (index == 6){
+    } else if (i == 6){
         console.log("******");
     }
 }
