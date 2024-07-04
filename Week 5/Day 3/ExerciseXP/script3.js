d784fbd2ee482cd691d6730882f2b2351bae3b8c
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-04 07:41:38 +0300

    w5d3

--- Week 5/Day 3/ExerciseXP/script3.js
+++ Week 5/Day 3/ExerciseXP/script3.js
@@ -1,4 +1,4 @@
-// Exercise 6.
+// Exercise 6. 
 
 let navBar = document.getElementById("navBar")
 navBar.setAttribute("id", "socialNetworkNavigation")
@@ -8,4 +8,22 @@ let textNode = document.createTextNode("Logout");
 newLi.appendChild(textNode)
 navBar.firstElementChild.appendChild(newLi)
 console.log(navBar.firstElementChild.firstElementChild);
-console.log(navBar.firstElementChild.lastElementChild)
+console.log(navBar.firstElementChild.lastElementChild);
+
+
+// Exercise 7.
+
+const allBooks = [{title: "Elon Musk",
+    author: "Walter Isaacson",
+    image : "https://m.media-amazon.com/images/I/81Kaj5++6pL._SL1500_.jpg",
+    alreadyRead : FALSE
+    }, 
+
+
+    {title: "Red Notice: A True Story of High Finance, Murder, and One Man's Fight for Justice",
+    author: "Bill Browder",
+    image: "https://m.media-amazon.com/images/I/81keU1kIh1L._SL1500_.jpg",
+    alreadyRead : TRUE
+    }]
+
+    
