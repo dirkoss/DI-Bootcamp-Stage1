886084f4fd4b0bbd2c751241c7825e5d0bea8f68
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-07 17:53:22 +0300

    W6D1

--- Week 6/Day 1/ExerciseXP/script.js
+++ Week 6/Day 1/ExerciseXP/script.js
@@ -1,4 +1,4 @@
-// Exercise 1 SCOPE
+// 🌟 Exercise 1 SCOPE
 
 // 1.1
 
@@ -30,4 +30,74 @@
     // because the variable is not reassigned in the function.
 
 
+// 🌟 Exercise 2 : Ternary Operator
+
+// function winBattle(){
+//     return true;
+// }
+
+// 2.1
+
+// winBattle => true
+
+// 2.2 & 2.3
+
+// let experiencePoints = winBattle() ? 10 : 1;
+
+// 2.3
+
+// console.log(experiencePoints);
+
+
+// 🌟 Exercise 3 : Is It A String ?
+
+// isString = (str) => typeof str == "string" ? console.log("True") : console.log("False");
+
+// 🌟 Exercise 4 : Find The Sum
+
+// let sum = (a, b) => typeof a == "number" && typeof b == "number" ? console.log(a + b) : console.log("Sorry but you can only add numbers");
+
+// 🌟  Exercise 5 : Kg And Grams
+
+
+// multiple optionalities, maybe in wrong order:
+
+// let calcWeight1 = (kg) => console.log(kg * 1000);
+
+// const calcWeight2(kg) {
+//     return kg * 1000;
+// }
+
+// function calcWeight3(kg) {
+//     return kg * 1000;
+// }
+
+// 🌟 Exercise 6 : Fortune Teller
+
+// (function(){ 
+//     let numChildren = 4;
+//     let partnerName = "Maria";
+//     let geoLocation = "Amsterdam";
+//     let jobTitle = "developer";
+//     console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numChildren} kids.`);
+// })();
+
+
+// 🌟 Exercise 7 : Welcome
+
+(function welcome = (name) => {
+    return `Welcome ${name}`;
+})("Dirk");
+
+
+// 🌟 Exercise 8 : Juice Bar nested functions
+
+The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
+
+
+function makeJuice(size) {
+    let size = small
+
+}
     
+}
