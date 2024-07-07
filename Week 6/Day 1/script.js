04f7644c5c4e93802c62376cdc672a3b4fccde6a
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-07 09:30:48 +0300

    summary

---
+++ Week 6/Day 1/script.js
@@ -0,0 +1,6 @@
+var num = 8;
+var num = 10; // let variable can be declared multiple times and is not limited to the function scope
+
+// let num = 8; // var variable cannot be declared multiple times but can be re-assigned a value and is limited to the function scope
+// let num = 10;
+console.log(num);
