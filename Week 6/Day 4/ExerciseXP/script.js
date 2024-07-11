f5936a8f7e9cd9bc5c4ce7c6982f1136f755d332
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-11 09:28:22 +0300

    W6D4

---
+++ Week 6/Day 4/ExerciseXP/script.js
@@ -0,0 +1,31 @@
+// 🌟 Exercise 3 
+
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+let jsonObj = JSON.stringify(marioGame);
+    console.log(jsonObj);
+    
+let jsonObj1 = JSON.stringify(marioGame,null,2);
+console.log(jsonObj1);
