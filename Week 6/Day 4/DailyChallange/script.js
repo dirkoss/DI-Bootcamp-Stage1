dd14516778df752f8092d58dc528d822000ca6f1
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-11 15:38:31 +0300

    Update script.js

--- Week 6/Day 4/DailyChallange/script.js
+++ Week 6/Day 4/DailyChallange/script.js
@@ -1,17 +1,28 @@
-const form = document.querySelector('form');
-document.getElementById('submit').addEventListener('click', handleSubmit)
+// const form = document.querySelector('form');
+// document.getElementById('submit').addEventListener('click', handleSubmit)
 
-function handleSubmit(e){
-    e.preventDefault();
+// function handleSubmit(e){
+//     e.preventDefault();
 
-    const form = document.querySelector("form");
-    const formData = new FormData(form);
+//     const form = document.querySelector("form");
+//     const formData = new FormData(form);
 
-    const data = Object.fromEntries(formData.entries());
+//     const data = Object.fromEntries(formData.entries());
 
-    const stringified = JSON.stringify(data);
+//     const stringified = JSON.stringify(data);
 
-    const element = document.createElement("p");
-    element.innerText = stringified;
-    document.querySelector("body").appendChild(element);
-}
+//     const element = document.createElement("p");
+//     element.innerText = stringified;
+//     document.querySelector("body").appendChild(element);
+// }
+
+let bobsFollowers = ['bob','hans','kees','peter']
+let tinasFollowers = ['kees', 'piet','peter']
+
+let mutualFollowers = []
+
+for (let i = 0; i < bobsFollowers.length; i++){
+  for (let j = 0; j < tinasFollowers.length; j++){
+   if (tinasFollowers[j] === bobsFollowers[i]){mutualFollowers.push(tinasFollowers[j])}}}
+
+console.log(mutualFollowers)
