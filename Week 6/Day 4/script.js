1970a19a642e308ef47b61e7aa3087d3a46f6cb9
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-11 08:57:30 +0300

    W6D5

--- Week 6/Day 4/script.js
+++ Week 6/Day 4/script.js
@@ -1,18 +1,23 @@
-let raceNumber = Math.floor(Math.random() * 1000);
+// let raceNumber = Math.floor(Math.random() * 1000);
 
-let earlyReg = true;
+// let earlyReg = true;
 
-let childAge = 14;
+// let childAge = 14;
 
-if (earlyReg == 'true' && childAge > 18) {
-  raceNumber += 1000;}
+// if (earlyReg == 'true' && childAge > 18) {
+//   raceNumber += 1000;}
 
-if (earlyReg == 'true' && childAge > 18) {
-  console.log(`Your starttime is 0930 and your startnumber is ${raceNumber}`)
-} 
-else if (earlyReg !== 'true' && childAge > 18) {
-  console.log(`Late adults run at 11:00 am Your starttime is 0930 and your startnumber is ${raceNumber}`)};
+// if (earlyReg == 'true' && childAge > 18) {
+//   console.log(`Your starttime is 0930 and your startnumber is ${raceNumber}`)
+// } 
+// else if (earlyReg !== 'true' && childAge > 18) {
+//   console.log(`Late adults run at 11:00 am Your starttime is 0930 and your startnumber is ${raceNumber}`)};
   
-else if (childAge < 18) {
-  console.log(`Youth registrants run at 12:30`)
-}
+// else 
+//   console.log(`Youth registrants run at 12:30`)
+
+
+// const numbers = [1, 2, 3];
+// const [a, b, c] = numbers;
+// console.log(a + b + c);
+const person = { name: "John", age: 25, gender: "Male" };
