af9cca9f18baad920bb13219e4fbbd8ac0b77ba5
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-14 11:02:59 +0300

    commit

--- Week 6/Day 5/ExerciseXP/script.js
+++ Week 6/Day 5/ExerciseXP/script.js
@@ -1,1 +1,25 @@
-f
+
+const flip = () => {
+    const coin = Math.floor(Math.random() * 2);
+    return coin === 0 ? 'heads' : 'tails';
+}
+
+console.log(flip());
+
+const flipcoin = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const flipresult = flip();
+        if (flipresult === 'heads') {
+            resolve('You win'+ flipcoin);
+        } else {
+            reject('You lose' + flipcoin);
+        }
+    }, 2000);
+
+});
+
+flipcoin.then((result) => {
+    console.log(result);
+}
+).catch((error) => { console.log(error) }
+);
