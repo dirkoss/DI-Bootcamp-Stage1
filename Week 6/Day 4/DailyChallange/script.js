9d1b73f3c87b25001ffbe5d3afa3f88b7e45907d
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-14 10:57:02 +0300

    commit

--- Week 6/Day 4/DailyChallange/script.js
+++ Week 6/Day 4/DailyChallange/script.js
@@ -28,12 +28,11 @@
 // console.log(mutualFollowers)
 
 for (let i = 0; i < 99; i++) {
-    if (i > 2 ) {
-       break;
-    }
-    console.log('Banana.');
-  }
-  
-  console.log('Orange you glad I broke out the loop!');
-  
-  
+   if (i > 2) {
+      break;
+   }
+   console.log('Banana.');
+}
+
+console.log('Orange you glad I broke out the loop!');
+
