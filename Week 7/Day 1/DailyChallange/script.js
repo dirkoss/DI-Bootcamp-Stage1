6063037a2a54ae6e9a61fa9fd9b28be3f1177fd1
Dirk Ultee <168425810+dirkoss@users.noreply.github.com>
2024-07-15 08:37:25 +0300

    Update script.js

--- Week 7/Day 1/DailyChallange/script.js
+++ Week 7/Day 1/DailyChallange/script.js
@@ -96,27 +96,7 @@ const morse = `{
     ")": "-.--.-"
   }`
 
-//   Create three functions. The two first functions should return a promise..
 
-// The first function is named toJs():
-// this function converts the morse json string provided above to a morse javascript object.
-// if the morse javascript object is empty, throw an error (use reject)
-// else return the morse javascript object (use resolve)
-
-// The second function called toMorse(morseJS), takes one argument: the new morse javascript object.
-
-// This function asks the user for a word or a sentence.
-// if the user entered a character that doesn’t exist in the new morse javascript object, throw an error. (use reject)
-// else return an array with the morse translation of the user’s word.
-// if the user enters the word "Hello", the promise resolves with this value ["....", ".", ".-..", ".-..","---"]
-// if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object
-
-
-// The third function called joinWords(morseTranslation), takes one argument: the morse translation array
-
-// this function joins the morse translation by using line break and display it on the page (ie. On the DOM)
-
-// Chain the three functions.
 
 let morseJS = JSON.parse(morse); // convert the morse string to a javascript object
 
@@ -167,3 +147,14 @@ function toJs( ) {
         }
     });
 }
+
+const joinMorseWords = (morseTranslation) => {
+    const para = document.createElement("p");
+    para.innerText = morseTranslation.join('\n');;
+    document.body.appendChild(para);
+}
+
+toJs()
+.then((obj) => toMorse(obj))
+.then((result) => joinWords(result))
+.catch(error => console.log(error))
